b4388e9b49e714f31ab15f035debf9173da28c5c
Sammy <samanthabeltran1993@gmail.com>
2024-06-05 17:57:47 -0700

    done

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operator = "";
 let result = 0;
 
@@ -27,64 +27,86 @@ const render = () => {
 const updateResult = () => {
   result = num1
 render()
-}
+};
 
 const updateNumbers = (event) => {
-  console.log(event.target.textContent)
-// if i have the number(textContent) -> how do i update the state?
-// check status  of what operator is
-if(!num1) {
-  num1 = event.target.textContent
-} else {
-  num1 += event.target.textContent
+  // console.log(event.target.textContent)
+
+if(num1 && operator) {
+// console.log('updating num2', event.target.textContent)
+
+  num2 += event.target.textContent;
+  result = num2
 } 
-// num1 is filled when no operator but num2 is filled out when operator is 
-// num2 will have same if statement
-updateResult()
+else {
+  num1 += event.target.textContent;
+  result = num1
+ }
+//  console.log(num1, num2)
+ render()
 }
+
 // i dont understand what this if statement is doing ^^
 
-const updateOperators = (event) => {
-  console.log(event.target.textContent)
-
+const resetCalc = () => {
+  num1 = '';
+  num2 = '';
+  operator = '';
+  result = 0;
 }
+
+const updateOperators = (event) => {
+  operator = event.target.textContent
+  if (operator === 'C'){
+    num1 = "";
+    num2 = "";
+    operator = "";
+    result = 0;
+    render()
+  }
+};
 // set variable oper to event.target.textConent
 // refrence the operator
 const updateEquals = (event) => {
   if (operator === '+')  {
-    output = (num1+num2)
+    result = (Number(num1)+Number(num2))
 }
 if (operator === '-')  {
-    output = (num1-num2)
+    result = (Number(num1)-Number(num2))
 }
 if (operator === '*')  {
-    output = (num1*num2)  
+    result = (Number(num1)*Number(num2)) 
 }
 if (operator === '/')  {
-    output = (num1/num2)   
+    result = (Number(num1)/Number(num2))   
 }    
-/* 
+/* (Number(num1)+Number(num2))^
 if (operator === 'C') {
   output = function clr() { */
-  console.log(event.target.textContent)
+  // console.log(event.target.textContent)
   render()
-  updateOperators()
 }
 
+const handleCalcuate = () => {
+  console.log(`${num1} ${operator} ${num2}`)
+  if(operator === '+'){
+    updateResult(Number(num1)+Number(num2))
+  }
+}
 
 numberBtnEls.forEach((numberBtnEl) =>  {
- numberBtnEl.addEventListener('click', updateNumbers)
+ numberBtnEl.addEventListener('click', updateNumbers);
 })
 // iterate over all operators
 // assign - listener (updateOperator)
 // update the operator variable with the textContent
 
 operatorBtnEls.forEach((operatorBtnEl) => {
-  operatorBtnEl.addEventListener('click', updateOperators)
+  operatorBtnEl.addEventListener('click', updateOperators);
 // my iteration over operators
 })
 
 equalBtnEl.addEventListener('click', updateEquals);
-
+// resetCalc.addEventListener('click', updateResult);
 
 render()
